9bb48ae7bb3941b61746234a3d8036c896c061ab
Mariana Murillo <mariana@primarix.com>
2024-04-27 11:24:30 -0600

    Filters: Bugs. Aún no está funcional

--- footloose/src/components/filters/filters.tsx
+++ footloose/src/components/filters/filters.tsx
@@ -1,28 +1,107 @@
-import { Dropdown } from "react-bootstrap";
+import { CustomInput, FormGroup } from "react-bootstrap";
 import "./styles.scss";
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from "recoil";
+import { productState } from "../../atoms/productState";
+import { categoryState } from "../../atoms/categoryState";
+import { brandState } from "../../atoms/brandState";
+import { products } from "../../utils/data";
 
 interface FilterProps {
     children: React.ReactNode;
 }
 
 const Filter: FC<FilterProps> = ({children}) => {
-    
-    return(
-        <>
-        <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-            Filter by category
-        </Dropdown.Toggle>
+    const allProducts = products();
+    const [productList, setProductList] = useRecoilState(productState);
+    const categoryList = useRecoilValue<string[]>(categoryState);
+    const brandList = useRecoilValue<string[]>(brandState);
+    const [selectedCategories, setSelectedCategories] = useState({});
+    const [selectedBrands, setSelectedBrands] = useState({});
 
-        <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-        </Dropdown>
-        {children}
-        </>
+    const handleCategoryCheck = (category: string, isChecked: boolean) => {
+      setSelectedCategories({
+        ...selectedCategories,
+        [category]: isChecked,
+      });
+      // Update "all" category filter based on selections
+      const allCategoryFilter = Object.keys(selectedCategories).every((category) => category !== "all" && !selectedCategories[category]);
+      setSelectedCategories({ ...selectedCategories, all: allCategoryFilter });
+    };
+
+    const handleBrandCheck = (brand: string, isChecked: boolean) => {
+      setSelectedBrands({
+        ...selectedBrands,
+        [brand]: isChecked,
+      });
+
+      const allBrandFilter = Object.keys(selectedBrands).every((brand) => selectedBrands[brand] === false);
+      setSelectedBrands({ ...selectedBrands, all: allBrandFilter });
+    };
+
+    useEffect(() => {
+      // Initialize selectedCategories with all categories set to false
+      const initialCategories = categoryList.reduce((acc, category) => {
+        return { ...acc, [category]: false };
+      }, {});
+
+      setSelectedCategories({ all: true, ...initialCategories });
+
+      const initialBrands = brandList.reduce((acc, brand) => {
+        return { ...acc, [brand]: false };
+      }, {});
+
+      setSelectedBrands({ all: true, ...initialBrands });
+
+    }, []);
+
+    useEffect(() => {
+      // Handle changes in selected filters
+      const filteredProducts = allProducts.filter((product) => {
+        const selectedCategoriesCopy = { ...selectedCategories };
+        delete selectedCategoriesCopy.all;
+  
+        const isCategoryMatch =
+          Object.keys(selectedCategoriesCopy).length === 0 ||
+          Object.keys(selectedCategoriesCopy).some((category) => product.category === category && selectedCategoriesCopy[category]);
+  
+        const isBrandMatch =
+          Object.keys(selectedBrands).length === 0 ||
+          selectedBrands[product.brand];
+  
+        return isCategoryMatch && isBrandMatch;
+      });
+  
+      setProductList(filteredProducts);
+    }, [selectedCategories, selectedBrands]);
+
+    return(
+      <div className="filter">
+        <p>Filter by category:</p>
+        {categoryList.map((category) => (
+          <div key={category}>
+            <input
+              type="checkbox"
+              id={`category-${category}`}
+              checked={selectedCategories.has(category)}
+              onChange={(event) => handleCategoryCheck(category, event.target.checked)}
+            />
+            <label htmlFor={`category-${category}`}>{category}</label>
+          </div>
+        ))}
+        <p>Filter by brand:</p>
+        {brandList.map((brand) => (
+          <div key={brand}>
+            <input
+              type="checkbox"
+              id={`brand-${brand}`}
+              checked={selectedBrands.has(brand)}
+              onChange={(event) => handleBrandCheck(brand, event.target.checked)}
+            />
+            <label htmlFor={`brand-${brand}`}>{brand}</label>
+          </div>
+        ))}
+      </div>
     );
 };
 
